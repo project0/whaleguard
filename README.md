c38deded2aa1b72cf372706a25be53a9af74fe20
Richard Hillmann <richie@project0.de>
2017-12-16 18:58:39 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# WhaleGuard
+
+WhaleGuard discovers docker to open dynamically your `ip6tables` firewall for your containers.
+The approach is to use the native v6 addresses of the container for inbound traffic without opening your whole subnet to the world.
+
+The containers are required to have an global ipv6 address.
+
+As the main focus is protecting the native `ipv6` subnet, ipv4 is currently not supported.
+
+## Usage
+
+```
+-defaults       Add some default deny rules
+-iface string   Incoming network interface on host (default "eth0")
+-network string Created chain just cares about this subnet
+```
+
+The docker client has some extra env variables you can set: `DOCKER_CERT_PATH`, `DOCKER_TLS_VERIFY`, `DOCKER_HOST`
+
+
+### Run in a docker container
+[Image DockerHub](https://hub.docker.com/r/project0de/whaleguard)
+
+The container requires some extended privileges (given by CAP_NET_ADMIN) and needs to be run on the host network.
+Obviously docker socket is required for api calls to docker.
+
+```bash
+docker run -ti --cap-add=NET_ADMIN --net host -v /var/run/docker.sock:/var/run/docker.sock project0de/whaleguard -iface eth0 -defaults -network 2001:1:1:/65
+```
+
+
+## Docker Labels
+
+To prevent automatic creation of unwanted rules, container needs to be labeled:
+
+### whaleguard
+
+The label `whaleguard` enables this container for whaleguard to use
+The value needs to be set to `true`
+
+### whaleguard.port
+
+The label `whaleguard.port` is optional to use.
+Per default whaleguard tries to open ports which are defined in the port mapping, if you dont want this behaviour use this label.
+
+In some situations its necessary to control better which ports needs to be opened, so you can define exactly which ports should be opened
+
+Setting the label like this `--labels="whaleguard.port=2003"` will open port 2003 to the containers v6 address.
+
+Multiple ports are allowed to define:  `--labels="whaleguard.port=2003,2004,2006"`
+
+If you want to specify the protocol (default is tcp):  `--labels="whaleguard.port=2003/tcp,2004,2006/udp"`
---
+++ main.go
@@ -0,0 +1,293 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/coreos/go-iptables/iptables"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/filters"
+	"github.com/docker/docker/client"
+)
+
+func main() {
+	IncomingInterface := flag.String("iface", "eth0", "Incoming network interface on host")
+	Subnet := flag.String("network", "::/0", "Created chain just cares about this subnet")
+	DefaultDenyRules := flag.Bool("defaults", false, "Add some default deny rules")
+	flag.Parse()
+
+	// check interface exists before starting
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		panic(err)
+	}
+	interfaceError := true
+	for _, i := range interfaces {
+		if i.Name == *IncomingInterface {
+			interfaceError = false
+		}
+	}
+
+	if interfaceError {
+		log.Fatal(fmt.Sprintf("Network interface not found: %s", *IncomingInterface))
+	}
+
+	_, ipnet, err := net.ParseCIDR(*Subnet)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("Could not parse network %s", *Subnet))
+	}
+
+	ipt, err := iptables.NewWithProtocol(iptables.ProtocolIPv6)
+	if err != nil {
+		panic(err)
+	}
+
+	fw := Firewall{
+		IPTables:  ipt,
+		ChainName: "WHALEGUARD",
+		Table:     "filter",
+		Interface: *IncomingInterface, // incoming interface before routing
+		Network:   ipnet,
+	}
+
+	// enforce older api version
+	os.Setenv("DOCKER_API_VERSION", "1.26")
+	cl, err := client.NewEnvClient()
+	if err != nil {
+		panic(err)
+	}
+	containerFilterArgs := filters.NewArgs(
+		filters.Arg("label", "whaleguard=true"))
+
+	if err = fw.InitializeChain(*DefaultDenyRules); err != nil {
+		panic(err)
+	}
+	log.Println("Synchronize with docker host")
+	container, err := cl.ContainerList(context.Background(), types.ContainerListOptions{
+		Filters: containerFilterArgs,
+	})
+	if err != nil {
+		panic(err)
+	}
+	for _, c := range container {
+		err := fw.AddContainer(c.ID, cl)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+
+	ctx := context.Background()
+
+	containerFilterArgs.Add("event", "start")
+	containerFilterArgs.Add("event", "die")
+	containerFilterArgs.Add("type", "container")
+	chanEvents, chanErrors := cl.Events(ctx, types.EventsOptions{
+		Filters: containerFilterArgs,
+	})
+	log.Println("Watch for container events")
+
+	for {
+		select {
+		case event := <-chanEvents:
+			switch event.Action {
+			case "start":
+				fw.AddContainer(event.ID, cl)
+			case "die":
+				log.Printf("Remove rules for container %s\n", event.Actor.ID)
+				fw.DropRules(event.Actor.ID)
+			default:
+				log.Printf("Unhandled event action: %s", event.Action)
+			}
+		case errors := <-chanErrors:
+			log.Panic(errors.Error())
+			return
+		}
+	}
+}
+
+// PortProto represents a Port with Protocol (udp/tcp)
+type PortProto struct {
+	Port  string
+	Proto string
+}
+
+// ParsePortProto parses a string combination (Port/Protocol) to create a new PortProto
+// Examples: 123/udp, 123
+func ParsePortProto(p string) (*PortProto, error) {
+	ppraw := strings.SplitN(p, "/", 2)
+	proto := "tcp"
+	if len(ppraw) == 2 {
+		proto = strings.ToLower(strings.TrimSpace(ppraw[1]))
+	}
+
+	if proto != "tcp" && proto != "udp" {
+		return nil, fmt.Errorf("unsupported protocol provided in label: %s", proto)
+	}
+	// ensure we got an integer
+	pi, err := strconv.Atoi(strings.TrimSpace(ppraw[0]))
+	if err != nil {
+		return nil, fmt.Errorf("could not parse port '%s' in label: %v", p, err)
+	}
+
+	return &PortProto{
+		Port:  strconv.Itoa(pi),
+		Proto: proto,
+	}, nil
+}
+
+// Firewall represents an iptables chain
+type Firewall struct {
+	IPTables  *iptables.IPTables
+	ChainName string
+	Table     string
+	Interface string
+	Network   *net.IPNet
+}
+
+// InitializeChain ensures the working chain is correctly setuped and clears old rules in the working chain
+func (f *Firewall) InitializeChain(defaults bool) error {
+	var err error
+	chains, err := f.IPTables.ListChains(f.Table)
+	if err != nil {
+		return err
+	}
+
+	exists := false
+	for _, a := range chains {
+		if a == f.ChainName {
+			exists = true
+		}
+	}
+	if !exists {
+		if err = f.IPTables.NewChain(f.Table, f.ChainName); err != nil {
+			return err
+		}
+	} else {
+		// Cleanup the chain to start fresh
+		if err = f.IPTables.ClearChain(f.Table, f.ChainName); err != nil {
+			return err
+		}
+	}
+
+	rulespec := []string{"-j", f.ChainName, "-i", f.Interface}
+	if f.Network != nil {
+		rulespec = append(rulespec, "--dst", f.Network.String())
+	}
+
+	// Ensure new chain will be used as first
+	err = f.InsertUnique("INPUT", rulespec...)
+	if err != nil {
+		return err
+	}
+
+	// initialize some default deny rules here?
+	if defaults {
+		// allow related traffic
+		f.IPTables.Append(f.Table, f.ChainName, "-m", "state", "--state", "ESTABLISHED,RELATED", "-j", "ACCEPT")
+		// allow v6 icmp packages
+		f.IPTables.Append(f.Table, f.ChainName, "-p", "ipv6-icmp", "-j", "ACCEPT")
+		// last but not least, reject everything else
+		f.IPTables.Append(f.Table, f.ChainName, "-j", "REJECT", "--reject-with", "icmp6-adm-prohibited")
+	}
+	return nil
+}
+
+// AddContainer retrieves the container information (ip and ports) and adds the specific allow rules
+func (f *Firewall) AddContainer(containerID string, client *client.Client) error {
+	info, err := client.ContainerInspect(context.Background(), containerID)
+	if err != nil {
+		return err
+	}
+
+	var ports []PortProto
+
+	if labelports, ok := info.Config.Labels["whaleguard.port"]; ok {
+		for _, p := range strings.Split(labelports, ",") {
+			pp, err := ParsePortProto(p)
+			if err != nil {
+				log.Print(err.Error())
+				continue
+			}
+			ports = append(ports, *pp)
+		}
+	} else {
+		// use exposed port
+		for k := range info.NetworkSettings.Ports {
+			ports = append(ports, PortProto{
+				Port:  k.Port(),
+				Proto: k.Proto(),
+			})
+		}
+	}
+	ip := info.NetworkSettings.GlobalIPv6Address
+	if len(ports) < 1 || ip == "" {
+		fmt.Println("No Ipv6 address or port assigned to this container")
+		return nil
+	}
+
+	for _, pp := range ports {
+		err := f.AddRule(info.ID, ip, pp)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+	return nil
+}
+
+// InsertUnique acts like IPTables.Insert except that it won't add a duplicate
+func (f *Firewall) InsertUnique(chain string, rulespec ...string) error {
+	exists, err := f.IPTables.Exists(f.Table, chain, rulespec...)
+	if err != nil {
+		return err
+	}
+	if !exists {
+		return f.IPTables.Insert(f.Table, chain, 1, rulespec...)
+	}
+	return nil
+}
+
+// AddRule adds a new allow rule for an container
+func (f *Firewall) AddRule(containerID string, ip string, pp PortProto) error {
+	rulespec := []string{
+		"-j", "ACCEPT",
+		"-p", pp.Proto,
+		"-d", ip,
+		"--dport", pp.Port,
+		"-m", "comment", "--comment", containerID,
+	}
+
+	log.Printf("Ensure rule: %v", rulespec)
+	return f.InsertUnique(f.ChainName, rulespec...)
+}
+
+// DropRules removes all allow rules for this container in the chain
+func (f *Firewall) DropRules(containerID string) error {
+	rules, err := f.IPTables.List(f.Table, f.ChainName)
+	if err != nil {
+		return err
+	}
+
+	var failed error
+	for _, r := range rules {
+		// just grep for the correct container
+		if strings.Contains(r, containerID) {
+			// lets make an rulespec out of it, first to parts is "-A Chainname"
+			rulespec := strings.Split(r, " ")
+			rulespec = rulespec[2:]
+
+			log.Printf("Delete rule: %v", rulespec)
+			err := f.IPTables.Delete(f.Table, f.ChainName, rulespec...)
+			if err != nil {
+				failed = err
+				log.Fatal(err)
+			}
+		}
+	}
+	return failed
+}
